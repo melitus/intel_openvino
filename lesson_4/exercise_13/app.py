ec190bf3145dbdabfdf2da2829f0ee7ebbdc78f5
melitus <asmelitus@gmail.com>
2020-02-14 16:08:26 +0100

    create app.py

--- lesson_4/exercise_13/app.py
+++ lesson_4/exercise_13/app.py
@@ -24,12 +24,18 @@ def get_args():
     optional = parser.add_argument_group('optional arguments')
 
     # -- Create the arguments
+    c_desc = "The color of the bounding boxes to draw; RED, GREEN or BLUE"
+    ct_desc = "The confidence threshold to use with the bounding boxes"
+    optional.add_argument("-c", help=c_desc, default='BLUE')
+    optional.add_argument("-ct", help=ct_desc, default=0.5)
+
     required.add_argument("-m", help=m_desc, required=True)
     optional.add_argument("-i", help=i_desc, default=INPUT_STREAM)
     optional.add_argument("-d", help=d_desc, default='CPU')
     args = parser.parse_args()
 
     return args
+
 def draw_boxes(frame, result, args, width, height):
     '''
     Draw bounding boxes onto the frame.
@@ -41,15 +47,33 @@ def draw_boxes(frame, result, args, width, height):
             ymin = int(box[4] * height)
             xmax = int(box[5] * width)
             ymax = int(box[6] * height)
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 0, 255), 1)
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), args.c, 3)
     return frame
 
+def convert_color(color_string):
+    '''
+    Get the BGR value of the desired bounding box color.
+    Defaults to Blue if an invalid color is given.
+    '''
+    colors = {"BLUE": (255,0,0), "GREEN": (0,255,0), "RED": (0,0,255)}
+    out_color = colors.get(color_string)
+    if out_color:
+        return out_color
+    else:
+        return colors['BLUE']
+    
 def infer_on_video(args):
+    # convet args for color and confidence
+    args.c = convert_color(args.c)
+    args.ct = float(args.ct)
+    
     ### TODO: Initialize the Inference Engine
     plugin = Network()
+
     ### TODO: Load the network model into the IE
     plugin.load_model(args.m, args.d, CPU_EXTENSION)
     net_input_shape = plugin.get_input_shape()
+
     # Get and open video capture
     cap = cv2.VideoCapture(args.i)
     cap.open(args.i)
@@ -62,7 +86,7 @@ def infer_on_video(args):
     # The second argument should be `cv2.VideoWriter_fourcc('M','J','P','G')`
     # on Mac, and `0x00000021` on Linux
     out = cv2.VideoWriter('out.mp4', 0x00000021, 30, (width,height))
-    
+
     # Process frames until the video ends, or process is exited
     while cap.isOpened():
         # Read the next frame
@@ -75,16 +99,18 @@ def infer_on_video(args):
         p_frame = cv2.resize(frame, (net_input_shape[3], net_input_shape[2]))
         p_frame = p_frame.transpose((2,0,1))
         p_frame = p_frame.reshape(1, *p_frame.shape)
+
         ### TODO: Perform inference on the frame
         plugin.async_inference(p_frame)
+
         ### TODO: Get the output of inference
         if plugin.wait() == 0:
             result = plugin.extract_output()
-        ### TODO: Update the frame to include detected bounding boxes
-        frame = draw_boxes(frame, result, args, width, height)
+            ### TODO: Update the frame to include detected bounding boxes
+            frame = draw_boxes(frame, result, args, width, height)
+            # Write out the frame
+            out.write(frame)
 
-        # Write out the frame
-        out.write(frame)
         # Break if escape key pressed
         if key_pressed == 27:
             break
